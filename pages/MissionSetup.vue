993c5c995c2a918e06e2b911ba1f969039979fa8
Navid Bonyadi <navidprinceofpersia@gmail.com>
2024-10-17 17:58:36 +0330

    create mission setup page

--- pages/MissionSetup.vue
+++ pages/MissionSetup.vue
@@ -1,13 +1,21 @@
 <script setup>
-
-
+import BackIcon from "@/components/shared/BackIcon.vue";
+import Logo from "@/components/shared/Logo.vue";
+import LongBackground from "@/components/shared/LongBackground.vue";
+import Button from "@/components/shared/Button.vue";
 </script>
 
 <template>
-  <div class="home-page">
- 
-      <div>MISSION</div>
- 
+  <div class="mission-page">
+    <div class="mission-head">
+      <Logo />
+      <BackIcon to="/" />
+    </div>
+    <LongBackground>
+      <div class="mission-btn">
+        <Button text="مرحله بعد" to="cardselection" />
+      </div>
+    </LongBackground>
   </div>
 </template>
 
