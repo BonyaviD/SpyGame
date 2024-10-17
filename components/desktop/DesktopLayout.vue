5cfe512795cdae630e13796cd89a7c819089bea1
Navid Bonyadi <navidprinceofpersia@gmail.com>
2024-10-17 14:26:04 +0330

    update desktop view, add qr code

--- components/desktop/DesktopLayout.vue
+++ components/desktop/DesktopLayout.vue
@@ -1,20 +1,34 @@
+<script setup>
+import QRcode from "@/assets/img/spy-qrcode.svg";
+import Logo from "../shared/Logo.vue";
+</script>
 <template>
-    <div class="desktop">
-      <p>You can use this site only via mobile</p>
-    </div>
-  </template>
-  
-  <style scoped>
-  .desktop {
-    width: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    position: fixed;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    font-size: 1.5rem;
-  }
-  </style>
-  
+  <div>
+    <Logo />
+  <div class="desktop">
+    <img :src="QRcode" alt="" />
+    <p>با موبایل وارد شوید</p>
+  </div>
+  </div>
+</template>
+
+<style scoped>
+.desktop {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 2rem;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 3.5rem;
+  margin-top: 3rem;
+}
+
+.desktop img {
+  width: 350px;
+}
+</style>
